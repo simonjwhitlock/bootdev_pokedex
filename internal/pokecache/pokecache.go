6d19d08cd049fc4189d14a883db9f926e0b3fdbf
simon whitlock <simonjwhitlock@gmail.com>
2025-09-25 19:30:57 +0100

    new internal package for cache defined. need to build

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,1 +1,20 @@
 package pokecache
+
+import (
+	"time"
+	"mutex"
+)
+
+type cacheEntry struct {
+	createdAt	time.Time
+	val			[]byte
+}
+
+type Cache map[string]cacheEntry {
+	protect		mutex
+	Cache		map[string]cacheEntry
+}
+
+func NewCache(interval time.Duration) {
+
+}
